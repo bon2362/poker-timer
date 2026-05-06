d10a66028915437e79b901d2b5c27b8f39d6c2ad
Evgenii Koshkin <bon2362@gmail.com>
2026-05-06 22:10:21 +0200

    chore: redirect www to apex domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.bsptimer.online',
+          },
+        ],
+        destination: 'https://bsptimer.online/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
